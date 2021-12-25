46a9a9b67159351fe29ab073ed548a281f9e0715
Геннадий Токарев <gtokarev@innoseti.ru>
2021-12-25 18:15:16 +0200

    Add install mongo to deploy

--- deploy.sh
+++ deploy.sh
@@ -16,3 +16,14 @@ puma -d
 if [ "$(ps aux | grep -Ice puma)" > 1 ]; then
     echo "Server has started"
         fi
+
+repo_location=" /etc/apt/sources.list.d"
+repo_conf="deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse"
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo $repo_conf | sudo tee $repo_location/mongodb-org-4.2.list
+sudo apt update -y
+sudo apt install -y mongodb-org
+sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
+sudo systemctl enable mongod && sudo systemctl start mongod
