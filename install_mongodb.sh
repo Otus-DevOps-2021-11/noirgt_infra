44d608da894d287d7b55ad8502d043b3203bc3aa
Геннадий Токарев <gtokarev@innoseti.ru>
2021-12-25 17:51:16 +0200

    Change TCP-socket for Mongo

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -8,5 +8,6 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add 
 echo $repo_conf | sudo tee $repo_location/mongodb-org-4.2.list
 sudo apt update -y
 sudo apt install -y $all_soft
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 
 sudo systemctl enable --now mongod
