9aad57966e5bd8951f3356c820da0f00b27830a6
Геннадий Токарев <gtokarev@innoseti.ru>
2021-12-25 17:23:07 +0200

    Add upgrade

--- install_ruby.sh
+++ install_ruby.sh
@@ -2,8 +2,9 @@
 
 all_soft="apt-utils ruby-full ruby-bundler build-essential"
 
-sudo apt update -y
+sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y $all_soft
+sudo gem install bundler
 
 if [ -n "$(ruby -v)" ] & [ -n "$(bundle -v)" ]; then
     echo "All soft has install"
