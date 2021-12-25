19161ee812ec276998e117be0a905e8172ac4c42
Геннадий Токарев <gtokarev@innoseti.ru>
2021-12-25 18:33:34 +0200

    Change version of Ubuntu in repo

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,11 @@
 
 project_url="https://github.com/express42/reddit.git"
 project_dir_name="reddit"
-repo_location=" /etc/apt/sources.list.d"
-repo_conf="deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse"
-all_soft=(git mongodb-org)
+all_soft=(git)
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-sudo echo $repo_conf | sudo tee $repo_location/mongodb-org-4.2.list
+
 sudo apt update -y
 sudo apt install -y $all_soft
-
-sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-sudo systemctl enable --now mongod
 
 mkdir ~/$project_dir_name
 git clone -b monolith $project_url ~/$project_dir_name
--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 repo_location=" /etc/apt/sources.list.d"
-repo_conf="deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse"
+repo_conf="echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list"
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo $repo_conf | sudo tee $repo_location/mongodb-org-4.2.list
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo $repo_conf | sudo tee $repo_location/mongodb-org-5.0.list
 sudo apt update -y
 sudo apt install -y mongodb-org
 sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
