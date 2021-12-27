7280082078251d6730ff63cd87ed4b8d4296a338
Геннадий Токарев <noirgt@mail.ru>
2021-12-27 21:32:20 +0200

    Remove upgrade system

--- config-scripts/install_ruby.sh
+++ config-scripts/install_ruby.sh
@@ -2,8 +2,7 @@
 
 all_soft="apt-utils ruby-full ruby-bundler build-essential"
 
-apt update -y && apt upgrade -y
-apt install -y $all_soft
+apt update -y && apt install -y $all_soft
 
 if [ -n "$(ruby -v)" ] & [ -n "$(bundle -v)" ]; then
     echo "All soft has install"
