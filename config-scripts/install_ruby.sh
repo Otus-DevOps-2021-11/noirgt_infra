6a43eb75be910b02b89714fc219d9c5450692dca
Геннадий Токарев <gtokarev@innoseti.ru>
2021-12-28 15:24:26 +0200

    Deleted lock files of apt

--- config-scripts/install_ruby.sh
+++ config-scripts/install_ruby.sh
@@ -2,7 +2,9 @@
 
 all_soft="apt-utils ruby-full ruby-bundler build-essential"
 
-apt update -y && apt install -y $all_soft
+sudo rm /var/lib/apt/lists/lock /var/cache/apt/archives/lock /var/lib/dpkg/lock*
+
+apt-get install -y $all_soft
 
 if [ -n "$(ruby -v)" ] & [ -n "$(bundle -v)" ]; then
     echo "All soft has install"
