c9ec50a3a371d1c7782c06abdd38c06f64b5bfbf
Геннадий Токарев <gtokarev@innoseti.ru>
2021-12-28 11:46:15 +0200

    Change directory of Reddit

--- config-scripts/deploy.sh
+++ config-scripts/deploy.sh
@@ -8,9 +8,9 @@ all_soft=(git)
 apt update -y
 apt install -y $all_soft
 
-mkdir ~/$project_dir_name
-git clone -b monolith $project_url ~/$project_dir_name
-cd ~/reddit && bundle install
+mkdir /opt/$project_dir_name
+git clone -b monolith $project_url /opt/$project_dir_name
+cd /opt/reddit && bundle install
 puma -d
 
 if [ "$(ps aux | grep -Ice puma)" > 1 ]; then
