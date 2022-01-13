c49b75733ecb97750fff2fe1e3118dc744f7d701
Геннадий Токарев <gtokarev@innoseti.ru>
2022-01-13 14:12:44 +0200

    Delete variable for lb

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,28 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.69.0"
+    }
+  }
+}
+
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
+}
+
+resource "yandex_vpc_network" "app-network" {
+  name = "reddit-app-network"
+}
+
+resource "yandex_vpc_subnet" "app-subnet" {
+  name           = "reddit-app-subnet"
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.app-network.id}"
+  v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
 resource "yandex_compute_instance" "app" {
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,3 @@ output "external_ip_address_app" {
 output "internnal_ip_address_app" {
   value = yandex_compute_instance.app.*.network_interface.0.ip_address
 }
-
-output "external_ip_address_lb" {
-  value = yandex_lb_network_load_balancer.lb.*.listener
-}
