ce5bf91687a5df724e538f6a7e8d69bbfd3b1a92
Геннадий Токарев <gtokarev@innoseti.ru>
2022-01-13 15:40:49 +0200

    Remove block's files

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 all_soft="mongodb-org"
-repo_location=" /etc/apt/sources.list.d"
+repo_location="/etc/apt/sources.list.d"
 repo_conf="deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse"
 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
 echo $repo_conf | tee $repo_location/mongodb-org-4.2.list
+
+sudo rm /var/lib/apt/lists/lock /var/cache/apt/archives/lock /var/lib/dpkg/lock*
 
 apt-get update -y
 apt-get install -y $all_soft
