29008463c392a11ea4d0bc57c53bbc34aa2b7513
Геннадий Токарев <gtokarev@innoseti.ru>
2022-01-14 09:37:19 +0200

    Add new variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -29,6 +29,7 @@ variable "private_key_path" {
 variable "image_id" {
   description = "Disk image"
 }
+
 variable "subnet_id" {
   description = "Subnet"
 }
@@ -45,4 +46,14 @@ variable "reddit_app_count" {
   description = "Count of Reddit application"
   # Значение по умолчанию
   default = 1
+}
+
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
+
+variable db_disk_image {
+description = "Disk image for reddit db"
+default = "reddit-db-base"
 }
