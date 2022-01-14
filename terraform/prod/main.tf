554f070fce5b1891d0c2511556a8e3d89f6b827d
Геннадий Токарев <gtokarev@innoseti.ru>
2022-01-14 17:56:38 +0200

    Add reddit module in main

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -5,6 +5,18 @@ terraform {
       version = "0.69.0"
     }
   }
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "noirgt-infra-bucket"
+    region     = "ru-central1"
+    key        = "terraform-states/ya-state.tfstate"
+    access_key = "vDW86CC49K_Hp4N4t1Rj"
+    secret_key = "PeQeTws-NfE6A3-YS8RVpJTtrzqM5DJsTONR_kaf"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
 
 provider "yandex" {
@@ -15,10 +27,11 @@ provider "yandex" {
 }
 
 module "app" {
-  source          = "../modules/app"
-  public_key_path = var.public_key_path
-  app_disk_image  = var.app_disk_image
-  subnet_id       = var.subnet_id
+  source           = "../modules/app"
+  public_key_path  = var.public_key_path
+  app_disk_image   = var.app_disk_image
+  subnet_id        = var.subnet_id
+  private_key_path = var.private_key_path
 }
 
 module "db" {
@@ -26,4 +39,11 @@ module "db" {
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
   subnet_id       = var.subnet_id
+}
+
+module "reddit" {
+  source                 = "../modules/reddit"
+  external_ip_address_app = module.app.external_ip_address_app
+  internal_ip_address_db = module.db.internal_ip_address_db
+  private_key_path       = var.private_key_path
 }
