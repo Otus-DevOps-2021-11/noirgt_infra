bf405daf27fedd346cd861a2c30779204fa65263
Геннадий Токарев <gtokarev@innoseti.ru>
2022-01-14 17:54:41 +0200

    Create new modules

---
+++ terraform/modules/reddit/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.69.0"
+    }
+  }
+}
+
+resource "null_resource" "reddit_deploy" {
+  connection {
+    type        = "ssh"
+    host        = var.external_ip_address_app
+    user        = "ubuntu"
+    agent       = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sed -i 's/127.0.0.1:27017/${var.internal_ip_address_db}:27017/g' /home/ubuntu/reddit/app.rb",
+      "sudo systemctl restart puma.service"
+    ]
+  }
+}
