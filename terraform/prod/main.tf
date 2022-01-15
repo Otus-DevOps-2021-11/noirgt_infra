19e41322aa35b62f6fea7defa9e0b24e544dbd3d
Геннадий Токарев <noirgt@mail.ru>
2022-01-15 15:29:58 +0200

    Delete backend section

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "noirgt-infra-bucket"
-    region     = "ru-central1"
-    key        = "terraform-states/ya-state.tfstate"
-    access_key = "vDW86CC49K_Hp4N4t1Rj"
-    secret_key = "PeQeTws-NfE6A3-YS8RVpJTtrzqM5DJsTONR_kaf"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
-}
-
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
@@ -19,11 +5,15 @@ provider "yandex" {
   zone                     = var.zone
 }
 
+module "vpc" {
+  source           = "../modules/vpc"
+}
+
 module "app" {
   source           = "../modules/app"
   public_key_path  = var.public_key_path
   app_disk_image   = var.app_disk_image
-  subnet_id        = var.subnet_id
+  subnet_id        = module.vpc.app-subnet.id
   private_key_path = var.private_key_path
 }
 
@@ -31,7 +21,7 @@ module "db" {
   source          = "../modules/db"
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
-  subnet_id       = var.subnet_id
+  subnet_id       = module.vpc.app-subnet.id
 }
 
 module "reddit" {
