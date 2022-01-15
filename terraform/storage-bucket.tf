ae8a0e8d6a9279a496d2c882ed6d1a74165b7672
Геннадий Токарев <noirgt@mail.ru>
2022-01-15 15:44:39 +0200

    Change S3 secret option to variables

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,11 +1,11 @@
 terraform {
-backend "s3" {
+  backend "s3" {
     endpoint   = "storage.yandexcloud.net"
     bucket     = "noirgt-infra-bucket"
     region     = "ru-central1"
     key        = "terraform-states/ya-state.tfstate"
-    access_key = "vDW86CC49K_Hp4N4t1Rj"
-    secret_key = "PeQeTws-NfE6A3-YS8RVpJTtrzqM5DJsTONR_kaf"
+    access_key = "${var.s3_access_key}"
+    secret_key = "${var.s3_secret_key}"
 
     skip_region_validation      = true
     skip_credentials_validation = true
