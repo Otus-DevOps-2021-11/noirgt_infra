2df03106be319d47006ab1eb16cf26d59ce5e4f0
Геннадий Токарев <noirgt@mail.ru>
2022-01-15 15:29:17 +0200

    Add config for storage bucket

---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,13 @@
+terraform {
+backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "noirgt-infra-bucket"
+    region     = "ru-central1"
+    key        = "terraform-states/ya-state.tfstate"
+    access_key = "vDW86CC49K_Hp4N4t1Rj"
+    secret_key = "PeQeTws-NfE6A3-YS8RVpJTtrzqM5DJsTONR_kaf"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
