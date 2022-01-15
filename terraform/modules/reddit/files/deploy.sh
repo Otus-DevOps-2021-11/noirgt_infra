d8a1add5baa29a7c9cc39b08a556422bbe5e0f26
Геннадий Токарев <noirgt@mail.ru>
2022-01-15 13:02:00 +0200

    Add recursion flag for "rm"

--- terraform/modules/reddit/files/deploy.sh
+++ terraform/modules/reddit/files/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 APP_DIR=${1:-$HOME}
-sudo rm /var/lib/apt/lists/lock /var/cache/apt/archives/lock /var/lib/dpkg/lock*
+sudo rm -rf /var/lib/apt/lists/lock /var/cache/apt/archives/lock /var/lib/dpkg/lock*
 sudo apt-get install -y git
 git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
 cd $APP_DIR/reddit
