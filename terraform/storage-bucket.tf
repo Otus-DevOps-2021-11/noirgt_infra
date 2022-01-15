df57d0570d6d4a932f7960e466ec9a0a8d7afd6a
Геннадий Токарев <noirgt@mail.ru>
2022-01-15 16:18:11 +0200

    Added variables for secret of S3

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -4,8 +4,8 @@ terraform {
     bucket     = "noirgt-infra-bucket"
     region     = "ru-central1"
     key        = "terraform-states/ya-state.tfstate"
-    access_key = "vDW86CC49K_Hp4N4t1Rj"
-    secret_key = "PeQeTws-NfE6A3-YS8RVpJTtrzqM5DJsTONR_kaf"
+    access_key = "${var.s3_access_key}"
+    secret_key = "${var.s3_secret_key}"
 
     skip_region_validation      = true
     skip_credentials_validation = true
